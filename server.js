351d019d4098a7c9eab8f56731bd8d9ffcdf1971
zain <saimzain1236@gmail.com>
2024-03-24 00:12:56 +0500

    fixed

--- server.js
+++ server.js
@@ -16,30 +16,32 @@ const cors = require("cors")
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 
-//api to load pdf
-app.get('/api/pdf/:foldername/:filename', (req, res) => {
-  const filename = req.params.filename;
-  const foldername = req.params.foldername;
-  
-  const filePath = path.join(__dirname,'PDF',foldername ,filename); // Assuming PDF files are stored in the 'pdfs' directory
-  console.log("here i am server filename is",filename,foldername)
-  try{
-      res.sendFile(filePath);
 
-  }
-  catch(error)
-  {
-    res.status(500).send({
-      message:"successfully",
-      data});
-  }
-
-});
 
 //production mode
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   
+  //api to load pdf
+  app.get('/api/pdf/:foldername/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const foldername = req.params.foldername;
+    
+    const filePath = path.join(__dirname,'PDF',foldername ,filename); // Assuming PDF files are stored in the 'pdfs' directory
+    console.log("here i am server filename is",filename,foldername)
+    try{
+        res.sendFile(filePath);
+
+    }
+    catch(error)
+    {
+      res.status(500).send({
+        message:"successfully",
+        data});
+    }
+
+  });
+
   app.get('*', (req, res) => {
     res.sendfile(path.join(__dirname = 'client/build/index.html'));
   })

580ecf889af6eb661553db0e60f33b4dee8a34c0
zain <saimzain1236@gmail.com>
2024-03-27 21:07:30 +0500

    check

--- server.js
+++ server.js
@@ -53,10 +53,10 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 
-// //build mode
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname+'/client/public/index.html'));
-// })
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/index.html'));
+})
 
  
   
